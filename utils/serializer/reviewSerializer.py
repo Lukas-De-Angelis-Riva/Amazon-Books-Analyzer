d1c4d3d06dc195f2e1f9980a84a9384f76c5f5bd
MauricioBuzzone <mbuzzone@fi.uba.ar>
2024-04-09 21:50:48 -0300

    reviewSerializer
    Co-Authored-By: Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>

---
+++ utils/serializer/reviewSerializer.py
@@ -0,0 +1,50 @@
+from utils.serializer.serializer import Serializer
+from utils.protocol import TlvTypes, SIZE_LENGTH
+from utils.protocol import string_to_bytes, string_from_bytes, integer_to_bytes, integer_from_bytes
+from utils.protocol import code_to_bytes
+from model.review import Review
+
+class ReviewSerializer(Serializer):
+
+    def make_raw_dict(self):
+        return {
+            TlvTypes.REVIEW_ID : b'',
+            TlvTypes.REVIEW_TITLE : b'',
+            TlvTypes.REVIEW_SCORE : b'',
+            TlvTypes.REVIEW_TEXT : b'',
+        }
+
+
+    def from_raw_dict(self, raw_dict):
+        # Verification: all reviews field should be received
+        assert raw_dict[TlvTypes.REVIEW_ID], "Invalid review: no id provided"
+        assert raw_dict[TlvTypes.REVIEW_TITLE], "Invalid review: no title provided"
+        assert raw_dict[TlvTypes.REVIEW_SCORE], "Invalid review: no score provided"
+        assert raw_dict[TlvTypes.REVIEW_TEXT], "Invalid review: no text provided"
+
+        return Review(
+            id = integer_from_bytes(raw_dict[TlvTypes.REVIEW_ID]),
+            title = string_from_bytes(raw_dict[TlvTypes.REVIEW_TITLE]),
+            score = integer_from_bytes(raw_dict[TlvTypes.REVIEW_SCORE]),
+            text = string_from_bytes(raw_dict[TlvTypes.REVIEW_TEXT]),
+        )
+
+    def to_bytes(self, chunk: list):
+        raw_chunk = b''
+
+        for book in chunk:
+            raw_book = b''
+            raw_book += integer_to_bytes(book.id, TlvTypes.REVIEW_ID)
+            raw_book += string_to_bytes(book.title, TlvTypes.REVIEW_TITLE)
+            raw_book += integer_to_bytes(book.score, TlvTypes.REVIEW_SCORE)
+            raw_book += string_to_bytes(book.text, TlvTypes.REVIEW_TEXT)
+            
+            raw_chunk += code_to_bytes(TlvTypes.REVIEW)
+            raw_chunk += int.to_bytes(len(raw_book), SIZE_LENGTH, 'big') 
+            raw_chunk += raw_book
+
+        result = code_to_bytes(TlvTypes.REVIEW_CHUNK)
+        result += int.to_bytes(len(chunk), SIZE_LENGTH, 'big') 
+        result += raw_chunk
+
+        return result
--- utils/test/test_utils.py
+++ utils/test/test_utils.py
@@ -2,7 +2,9 @@ import io
 import unittest
 
 from model.book import Book
+from model.review import Review
 from utils.serializer.bookSerializer import BookSerializer
+from utils.serializer.reviewSerializer import ReviewSerializer
 
 class TestUtils(unittest.TestCase):
     def test_serializer_can_packet_two_books_in_the_same_mesagge(self):
@@ -42,6 +44,40 @@ class TestUtils(unittest.TestCase):
         assert book2.publisher == _book2.publisher
         assert book2.publishedDate == _book2.publishedDate
         assert book2.categories == _book2.categories
+
+    def test_serializer_can_packet_two_review_in_the_same_mesagge(self):
+        serializer = ReviewSerializer()
+
+        review1 = Review(
+            id=1,
+            title='At the Montains of madness',
+            score=5,
+            text="mu' bueno",
+        )
+
+        review2 = Review(
+            id=2,
+            title='The C programming language',
+            score=3,
+            text="int x80",
+        )
+
+        chunk = serializer.to_bytes([review1, review2])
+        reader = io.BytesIO(chunk)
+        serial = serializer.from_chunk(reader)
+
+        _review1 = serial[0]
+        _review2 = serial[1]
+
+        assert review1.id == _review1.id
+        assert review1.title == _review1.title
+        assert review1.score == _review1.score
+        assert review1.text == _review1.text
+
+        assert review2.id == _review2.id
+        assert review2.title == _review2.title
+        assert review2.score == _review2.score
+        assert review2.text == _review2.text
 
 if __name__ == '__main__':
     unittest.main()
