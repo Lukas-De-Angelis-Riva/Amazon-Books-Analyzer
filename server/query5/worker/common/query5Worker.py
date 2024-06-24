f4fa627b842862d8fb1a4c8b620ec2677a87f7d3
Lukas DAR <60785752+Lukas-De-Angelis-Riva@users.noreply.github.com>
2024-06-24 11:01:23 -0300

    test: failover worker (query5)
    Y con esto se terminaron los Workers!

--- server/query5/worker/common/query5Worker.py
+++ server/query5/worker/common/query5Worker.py
@@ -1,13 +1,16 @@
 import logging
 import io
 
-from utils.worker import Worker, ClientTracker, TOTAL
+from utils.worker import Worker, TOTAL
 from utils.middleware.middleware import Middleware, ACK, NACK
 from dto.q5Partial import Q5Partial
 from utils.serializer.q5ReviewInSerializer import Q5ReviewInSerializer  # type: ignore
 from utils.serializer.q5PartialSerializer import Q5PartialSerializer    # type: ignore
 from utils.serializer.q5BookInSerializer import Q5BookInSerializer      # type: ignore
 from utils.model.message import Message, MessageType
+
+# TEST PURPOSES
+from utils.model.virus import virus
 
 
 def IN_BOOKS_QUEUE_NAME(peer_id):
@@ -22,16 +25,13 @@ def OUT_QUEUE_NAME():
     return 'Q5-Sync'
 
 
-class BookTracker(ClientTracker):
-    def __init__(self, client_id):
-        super().__init__(client_id)
-        self.n_books = 0
-        self.all_books_received = False
+N_BOOKS = "N_BOOKS"
+ALL_BOOKS_RECEIVED = "ALL_BOOKS_RECEIVED"
 
 
 class Query5Worker(Worker):
-    def __init__(self, category, peer_id, peers, chunk_size):
-        middleware = Middleware()
+    def __init__(self, category, peer_id, peers, chunk_size, test_middleware=None):
+        middleware = test_middleware if test_middleware else Middleware()
 
         middleware.consume(queue_name=IN_BOOKS_QUEUE_NAME(peer_id), callback=self.recv_book)
         middleware.consume(queue_name=IN_REVIEWS_QUEUE_NAME(peer_id), callback=self.recv)
@@ -45,6 +45,16 @@ class Query5Worker(Worker):
         self.category = category.lower()
         self.book_serializer = Q5BookInSerializer()
 
+        self.recovery()
+
+    def adapt_tracker(self):
+        if N_BOOKS not in self.tracker.meta_data:
+            self.tracker.meta_data[N_BOOKS] = 0
+        if ALL_BOOKS_RECEIVED not in self.tracker.meta_data:
+            self.tracker.meta_data[ALL_BOOKS_RECEIVED] = False
+
+        self.tracker.parser = Q5Partial.decode
+
     ###############
     # BOOK WORKER #
     ###############
@@ -54,29 +64,41 @@ class Query5Worker(Worker):
             logging.debug(f'action: new_book | result: saving | book: {book}')
             self.tracker.data[book.title] = Q5Partial(book.title)
 
-    def recv_raw_book(self, raw):
+    def recv_raw_book(self, raw, chunk_id):
         reader = io.BytesIO(raw)
         input_chunk = self.book_serializer.from_chunk(reader)
         logging.debug(f'action: new_chunk | chunck_len: {len(input_chunk)}')
         for input in input_chunk:
             self.save_book(input)
-        self.tracker.n_books += len(input_chunk)
+        virus.infect()
+        self.tracker.persist(chunk_id, flush_data=True,
+                             N_BOOKS=self.tracker.meta_data[N_BOOKS]+len(input_chunk))
+        virus.infect()
 
     def recv_book(self, raw_msg, key):
         msg = Message.from_bytes(raw_msg)
-        if msg.client_id not in self.clients:
-            self.clients[msg.client_id] = BookTracker(msg.client_id)
-        self.tracker = self.clients[msg.client_id]
+        if msg.client_id in self.worked_clients:
+            return ACK
+
+        self.context_switch(msg.client_id)
+
+        if msg.ID in self.tracker.worked_chunks:
+            return ACK
 
         if msg.type == MessageType.EOF:
-            if msg.args[TOTAL] != self.tracker.n_books:
-                logging.debug(f'action: recv_book_eof | remaining {msg.args[TOTAL]-self.tracker.n_books} left')
+            if msg.args[TOTAL] != self.tracker.meta_data[N_BOOKS]:
+                virus.infect()
+                diff = msg.args[TOTAL]-self.tracker.meta_data[N_BOOKS]
+                logging.debug(f'action: recv_book_eof | remaining: {diff} left')
                 return NACK
             else:
+                virus.infect()
+                self.tracker.persist(msg.ID, ALL_BOOKS_RECEIVED=True)
+                virus.infect()
                 logging.debug('action: recv_book_eof | success | all_books_received')
-                self.tracker.all_books_received = True
                 return ACK
-        self.recv_raw_book(msg.data)
+        self.recv_raw_book(msg.data, msg.ID)
+        virus.infect()
         return ACK
 
     #################
@@ -85,11 +107,12 @@ class Query5Worker(Worker):
 
     def recv(self, raw_msg, key):
         msg = Message.from_bytes(raw_msg)
-        if msg.client_id not in self.clients:
-            self.clients[msg.client_id] = BookTracker(msg.client_id)
-        self.tracker = self.clients[msg.client_id]
-        if not self.tracker.all_books_received:
+        if msg.client_id in self.worked_clients:
+            return ACK
+        self.context_switch(msg.client_id)
+        if not self.tracker.meta_data[ALL_BOOKS_RECEIVED]:
             logging.debug('action: recv_raw | status: not_all_books_received | NACK')
+            virus.infect()
             return NACK
         return super().recv(raw_msg, key)
 
@@ -104,14 +127,21 @@ class Query5Worker(Worker):
         logging.debug(f'action: new_review | review: {review}')
         if review.title in self.tracker.data:
             logging.debug(f'action: new_review | result: update | review: {review}')
+            old = self.tracker.data[review.title].copy()
             self.tracker.data[review.title].update(review)
+            new = self.tracker.data[review.title].copy()
+
+            self.tracker.log_manager.hold_change(review.title, old, new)
         return
 
     def do_after_work(self, chunk_id):
         return
 
-    def send_results(self):
-        n = len(self.tracker.data)
-        self.tracker.results = {k: v for k, v in self.tracker.data.items() if v.n > 0}
-        logging.debug(f'action: filtering_result | result: success | n: {n} >> {len(self.tracker.results)}')
-        super().send_results()
+    def terminator(self):
+        results = [v for v in self.tracker.data.values() if v.n > 0]
+        if results:
+            logging.debug(f'action: filtering_result | result: success | n: {len(self.tracker.data)} >> {len(results)}')
+            virus.infect()
+            self.send_results(results)
+        virus.infect()
+        self.send_eof(len(results))
